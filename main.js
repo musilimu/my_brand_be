ae8d8ba7794ceafc31ad58e90d6619ab95600af3
muslim uwitondanishema <muslimuwitondanishema@gmail.com>
2022-12-23 13:31:03 +0200

    fixed cyclic deployment issues

---
+++ main.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import app from './src/index.js'
+import swagerDocs from './src/utils/swagger.js'
+const server = express()
+server.use(app)
+server.listen(process.env.port || 3000, () => {
+  swagerDocs(server, 3000)
+  console.log(`server started listening on port ${process.env.port || 3000}`)
+})
